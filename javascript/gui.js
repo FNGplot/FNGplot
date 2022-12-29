7e2b1900c64a0e8bb0bcc453621779600ca95175
Wish Lin <wish.rirf@gmail.com>
2022-12-29 19:32:49 +0800

    change indentation style

--- javascript/gui.js
+++ javascript/gui.js
@@ -12,46 +12,46 @@ function toggleLeftPanel(value){
 
 //toggling
 function toggleToolbar(optn){
-	let n = document.querySelectorAll("button[id^=\"toolbar-select-\"]");                       //perform an "and" selection and select all 9 buttons
+    let n = document.querySelectorAll("button[id^=\"toolbar-select-\"]");                       //perform an "and" selection and select all 9 buttons
     n.forEach((btn) => {
         btn.style.background = "transparent";                                                   //set all buttons to transparent background (unselected)
-		btn.style.color = "#000000";                                                            //set all buttons' text to black (unselected)
-		btn.style.fontWeight = "normal";                                                        //set font weight to normal (unselected)
+        btn.style.color = "#000000";                                                            //set all buttons' text to black (unselected)
+        btn.style.fontWeight = "normal";                                                        //set font weight to normal (unselected)
     });
-	n[optn].style.background = TOOLBAR_CLR[optn];                                               //set background to its border color (selected)
-	n[optn].style.color = "#ffffff";                                                            //set text color to white (selected)
-	n[optn].style.fontWeight = "bold";                                                          //set font weight to bold (selected)
-	
-	n = document.querySelectorAll("div[id^=\"toolbar-item-\"]");                  //perform an "and" selection and select all 9 divs
+    n[optn].style.background = TOOLBAR_CLR[optn];                                               //set background to its border color (selected)
+    n[optn].style.color = "#ffffff";                                                            //set text color to white (selected)
+    n[optn].style.fontWeight = "bold";                                                          //set font weight to bold (selected)
+    
+    n = document.querySelectorAll("div[id^=\"toolbar-item-\"]");                  //perform an "and" selection and select all 9 divs
     n.forEach((d) => {                                                                          //hide all divs first
         d.style.display = "none";
     });
-	n[optn].style.display = "block";                                                            //then show the selected div
+    n[optn].style.display = "block";                                                            //then show the selected div
 }
 
 //initializing
 function initToolbar(){
-	let n = document.querySelectorAll("button[id^=\"toolbar-select-\"]");
-	for(let i = 0;i<9;i++){
-		n[i].style.borderColor = TOOLBAR_CLR[i]; //initialize them to their respective colors
-		n[i].addEventListener("click", function() {toggleToolbar(i)}); //attach click eventlisteners
-	}
+    let n = document.querySelectorAll("button[id^=\"toolbar-select-\"]");
+    for(let i = 0;i<9;i++){
+        n[i].style.borderColor = TOOLBAR_CLR[i]; //initialize them to their respective colors
+        n[i].addEventListener("click", function() {toggleToolbar(i)}); //attach click eventlisteners
+    }
 }
 
 //Log system time
 function systemTime(){
-	console.log(`System Time: ${Date.now()-SYSTEM_EPOCH}ms\n`);
+    console.log(`System Time: ${Date.now()-SYSTEM_EPOCH}ms\n`);
 }
 
 /*Left Panel*/
 
 //Change root zoom level
 function changeRootZoom(value,mode){ //mode 0 = still dragging. 1 = release thumb.
-	if(mode){
-		document.getElementById("root-frame").style.transform=`scale(${value/100})`;
-		document.getElementById("rootzoom-label").innerHTML = `Zoom: ${value}%`;
-	}
-	else{
-		document.getElementById("rootzoom-label").innerHTML = `Zoom: ${value}%`;
-	}
+    if(mode){
+        document.getElementById("root-frame").style.transform=`scale(${value/100})`;
+        document.getElementById("rootzoom-label").innerHTML = `Zoom: ${value}%`;
+    }
+    else{
+        document.getElementById("rootzoom-label").innerHTML = `Zoom: ${value}%`;
+    }
 }
--- javascript/init.js
+++ javascript/init.js
@@ -3,52 +3,52 @@
 console.log(`SYSTEM_EPOCH: ${SYSTEM_EPOCH}`);
 
 document.querySelector("#left-panel-select").addEventListener("change", function(){
-	toggleLeftPanel(this.value);
+    toggleLeftPanel(this.value);
 });
 document.querySelector("#rootzoom-slider").addEventListener("input", function(){
-	changeRootZoom(this.value,0);
+    changeRootZoom(this.value,0);
 });
 document.querySelector("#rootzoom-slider").addEventListener("change", function(){
-	changeRootZoom(this.value,1);
+    changeRootZoom(this.value,1);
 });
-	
+
 initToolbar(); //initialize toolbar's positions, colors and click handlers
 toggleToolbar(1); //set it to "geometry" (default select)
-	
+    
 //Init sortable container (the only one present on onload should be #block-frame, but I'll keep this code for possible future changes)
 //NESTED_SORTABLES = [].slice.call(document.querySelectorAll('.nested-sortable')); //A weird but concise way to transfrom a NodeList into an Array
 SORTABLE_LIST.push(
-	new Sortable(document.querySelector("#block-frame"), {
-		group: 'block-frame',
-		animation: 150,
-		fallbackOnBody: true,
-		forceFallback: true,
-		onEnd: function (evt) {
-			if(!(evt.to.dataset.sid == evt.from.dataset.sid && evt.oldIndex == evt.newIndex)){  //If the position actually changed
-				moveObject(evt.item, evt.from.dataset.sid, evt.oldIndex+1, evt.to.dataset.sid, evt.newIndex+1);
-			}
-		},
-		ghostClass: 'ghost-class',
-		draggable: ".obj-block",
-		filter: ".folder",  //folders are not draggable
-		swapThreshold: 0.65,
-		scroll: true,
-		scrollSensitivity: 80,
-		scrollSpeed: 10
-	})
+    new Sortable(document.querySelector("#block-frame"), {
+        group: 'block-frame',
+        animation: 150,
+        fallbackOnBody: true,
+        forceFallback: true,
+        onEnd: function (evt) {
+            if(!(evt.to.dataset.sid == evt.from.dataset.sid && evt.oldIndex == evt.newIndex)){  //If the position actually changed
+                moveObject(evt.item, evt.from.dataset.sid, evt.oldIndex+1, evt.to.dataset.sid, evt.newIndex+1);
+            }
+        },
+        ghostClass: 'ghost-class',
+        draggable: ".obj-block",
+        filter: ".folder",  //folders are not draggable
+        swapThreshold: 0.65,
+        scroll: true,
+        scrollSensitivity: 80,
+        scrollSpeed: 10
+    })
 );
 
 window.addEventListener("error", function(){
-	alert("Execution Failed.");
+    alert("Execution Failed.");
 });
 
 window.addEventListener("keydown", function(event){
-	if(event.key == "F9"){ //F9: Run script
-		event.preventDefault();
-		console.log(window.innerWidth);
-		console.log(window.innerHeight);
-		console.log(document.documentElement.clientWidth);
-		console.log(document.documentElement.clientHeight);
-	}
+    if(event.key == "F9"){ //F9: Run script
+        event.preventDefault();
+        console.log(window.innerWidth);
+        console.log(window.innerHeight);
+        console.log(document.documentElement.clientWidth);
+        console.log(document.documentElement.clientHeight);
+    }
 });
 //-------Primary initializing sequence----------------------
