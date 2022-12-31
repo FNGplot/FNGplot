ef6175042148aa7a7dd94d4d31f2bd24ac4d9a18
Wish Lin <wish.rirf@gmail.com>
2022-12-31 10:54:22 +0800

    small update

--- javascript/block.js
+++ javascript/block.js
@@ -55,72 +55,73 @@ function changeVisibility(sid) {
 
 
 // Constructors
-function LinePP(sid) {
-    //------All attributes except sid have a default value before the user changes them.
-    //System
-    this.sid = sid;
-    this.name = "2-point line";
-    this.display = true;
-    this.updatePos = function () {
-        let avatar = BLOCK_FRAME.querySelector(`div[data-sid='${this.sid}']`); //Find this object's block avatar
+class LinePP {
+    constructor(sid) {
+        //------All attributes except sid have a default value before the user changes them.
+        //System
+        this.sid = sid;
+        this.name = "2-point line";
+        this.display = true;
+        this.update = function() {
+            let block = BLOCK_FRAME.querySelector(`div[data-sid='${this.sid}']`); //Find this object's draggable block
 
 
 
-        //s.setAttribute("y1",toRealY(this.xy).toString());
-        //s.setAttribute("x2",toRealX(this.x2).toString());
-        //s.setAttribute("y2",toRealY(this.y2).toString());
-    };
-    //Element
-    this.x1 = -5;
-    this.y1 = -4;
-    this.x2 = 5;
-    this.y2 = 2;
-    this.pathLength = 100;
-    //Style
-    this.lineWidth = 5;
-    this.lineCap = "round";
-    this.dashArray = '';
-    this.dashOffset = 0;
-    this.color = "#8a408b"; //Wisteria purple
-    this.opacity = 1;
+
+
+            //s.setAttribute("y1",toRealY(this.xy).toString());
+            //s.setAttribute("x2",toRealX(this.x2).toString());
+            //s.setAttribute("y2",toRealY(this.y2).toString());
+        };
+        //Element
+        this.x1 = -5;
+        this.y1 = -4;
+        this.x2 = 5;
+        this.y2 = 2;
+        this.pathLength = 100;
+        //Style
+        this.lineWidth = 5;
+        this.lineCap = "round";
+        this.dashArray = '';
+        this.dashOffset = 0;
+        this.color = "#8a408b"; //Wisteria purple
+        this.opacity = 1;
+    }
 }
 
 //Create a new FNGobject
-function createBlankObject(objName) { 
-    let sid = makeSID();
-    switch(objName){
-        case "LinePP": {                                             //LinePP will be the example here
-            // Step 1 of 3: FNGobject
-            OBJECT_LIST.push(new LinePP(sid));                      //creates a blank LinePP object and push it into array
+var createBlankObject = { 
+    linepp: function(){                                         //LinePP will be the example here
+        // Step 1 of 3: FNGobject
+        let sid = makeSID();
+        OBJECT_LIST.push(new LinePP(sid));                      //creates a blank LinePP object and push it into array
             
-            // Step 2 of 3: draggable block
-            let n = document.querySelector('#basic-block-template').content.firstElementChild.cloneNode(true); //copy a blank block template
-            n.classList.add('geo');                                 //adds geometry object class
-            n.querySelector('img').src = "svg/system/line_pp.svg";  //init the small icon
-            n.querySelector('input').value = "2-point line";        //display default name
-            n.dataset.sid = sid;                                    //assign this id-less block a data-id, in sync with the hidden object
-            BLOCK_FRAME.appendChild(n); //add the block to the UI
+        // Step 2 of 3: draggable block
+        let n = document.querySelector('#basic-block-template').content.firstElementChild.cloneNode(true); //copy a blank block template
+        n.classList.add('geo');                                 //adds geometry object class
+        n.querySelector('img').src = "svg/system/line_pp.svg";  //init the small icon
+        n.querySelector('input').value = "2-point line";        //display default name
+        n.dataset.sid = sid;                                    //assign this id-less block a data-id, in sync with the hidden object
+        BLOCK_FRAME.appendChild(n); //add the block to block frame
 
-            // Step 3 of 3: SVG element(s)
-            // I set the atttributes the SVG way and not the CSS way for easier export later on.
-            // As this varies greatly across different FNGobjects, I decided to set them up one by one and not use some property array trick.
-            let s = document.createElementNS("http://www.w3.org/2000/svg", 'line');
-            s.dataset.sid = sid;
-            s.dataset.name = "2-point line";
-            s.setAttribute("display"," "); // " " = true
-            s.setAttribute("x1",toRealX(-5).toString());
-            s.setAttribute("y1",toRealY(-4).toString());
-            s.setAttribute("x2",toRealX(5).toString());
-            s.setAttribute("y2",toRealY(2).toString());
-            s.setAttribute("pathLength","100");
-            s.setAttribute("stroke-width","5");
-            s.setAttribute("stroke-linecap","round");
-            s.setAttribute("stroke-dasharray","");
-            s.setAttribute("stroke-dashoffset","0");
-            s.setAttribute("stroke","#8a408b");
-            s.setAttribute("stroke-opacity","1");
-            SVG_CANVAS.appendChild(s);  //add the new SVG element to canvas
-            break;
-        }
+        // Step 3 of 3: SVG element(s)
+        // I set the atttributes the SVG way and not the CSS way for easier export later on.
+        // As this varies greatly across different FNGobjects, I decided to set them up one by one and not use some property array trick.
+        let s = document.createElementNS("http://www.w3.org/2000/svg", 'line');
+        s.dataset.sid = sid;
+        s.dataset.name = "2-point line";
+        s.setAttribute("display"," "); // " " = true
+        s.setAttribute("x1",toRealX(-5).toString());
+        s.setAttribute("y1",toRealY(-4).toString());
+        s.setAttribute("x2",toRealX(5).toString());
+        s.setAttribute("y2",toRealY(2).toString());
+        s.setAttribute("pathLength","100");
+        s.setAttribute("stroke-width","5");
+        s.setAttribute("stroke-linecap","round");
+        s.setAttribute("stroke-dasharray","");
+        s.setAttribute("stroke-dashoffset","0");
+        s.setAttribute("stroke","#8a408b");
+        s.setAttribute("stroke-opacity","1");
+        SVG_CANVAS.appendChild(s);  //add the new SVG element to canvas
     }
 }
