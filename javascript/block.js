6984574897a3a45d5b3b988b1f23f1a2abc6769f
Wish Lin <wish.rirf@gmail.com>
2023-01-01 13:23:44 +0800

    bugfix

--- javascript/block.js
+++ javascript/block.js
@@ -74,8 +74,8 @@ function initEditPanel(panelElem,sid){
         inputElem.value = obj[inputElem.dataset.property]; //get their respective properties and display them
     });
     panelElem.dataset.objtype = obj.constructor.name.toLowerCase();
-    BLOCK_FRAME.querySelector(`div[data-sid='${sid}']`).style.height = `${panelElem.clientHeight + 63}px`; //A workaround for transition. See https://css-tricks.com/using-css-transitions-auto-dimensions/ for why I resort to this hard-coded method.
-    //The magic number "63" is the size of margin-top(55) + margin-bottom(8)
+    BLOCK_FRAME.querySelector(`div[data-sid='${sid}']`).style.height = `${panelElem.offsetHeight + 65}px`; //A workaround for transition. See https://css-tricks.com/using-css-transitions-auto-dimensions/ for why I resort to this hard-coded method.
+    //The magic number "65" is the size of margin-top(55) + margin-bottom(10)
 }
 
 
