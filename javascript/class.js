20907c4eb68ee3d1bd85c3c5495a08fadbf36620
Wish Lin <wish.rirf@gmail.com>
2023-01-15 08:26:23 +0800

    new updateMath() structure updated

--- javascript/class.js
+++ javascript/class.js
@@ -69,12 +69,13 @@ class Rect extends StrokeFillParent {
         this.width = 7;
         this.height = 5;
         this.roundCorner = 0;
-        this.lineJoin = "miter";
+        this.SvgStyle.lineJoin = "miter";
         this.updateMath = function(svgElem){
-            svgElem.setAttribute("x1", toPxPosX(obj.x1));
-            svgElem.setAttribute("x2", toPxPosX(obj.x2));
-            svgElem.setAttribute("y1", toPxPosY(obj.y1));
-            svgElem.setAttribute("y2", toPxPosY(obj.y2));
+            svgElem.setAttribute("x", toPxPosX(this.originX - fngNS.RectOrigin[this.originHoriz] * this.width));
+            svgElem.setAttribute("y", toPxPosY(this.originY + fngNS.RectOrigin[this.originVert] * this.height));
+            svgElem.setAttribute("rx", this.roundCorner);
+            svgElem.setAttribute("width", toPxLenX(this.width));
+            svgElem.setAttribute("height", toPxLenY(this.height));
         }
     }
 }
@@ -85,6 +86,12 @@ class Circle extends StrokeFillParent {  // Actually uses an SVG <ellipse> in ca
         this.cx = 2;
         this.cy = 4;
         this.radius = 2.5;
+        this.updateMath = function(svgElem){
+            svgElem.setAttribute("cx", toPxPosX(this.cx));
+            svgElem.setAttribute("cy", toPxPosY(this.cy));
+            svgElem.setAttribute("rx", toPxLenX(this.radius));
+            svgElem.setAttribute("ry", toPxLenY(this.radius));
+        }
     }
 }
 class Circle3P extends StrokeFillParent {
@@ -97,35 +104,35 @@ class Circle3P extends StrokeFillParent {
         this.y2 = 3;
         this.x3 = -2;
         this.y3 = 6;
-    }
-    updateMath(svgElem){
-        // Calculate circumcenter from three points
-        // Formula: https://blog.csdn.net/liyuanbhu/article/details/52891868
-        const [a, b, c, d, e, f] = 
-            [   
-                this.x1 - this.x2,
-                this.y1 - this.y2,
-                this.x1 - this.x3,
-                this.y1 - this.y3,
-                0.5 * ((this.x1**2 - this.x2**2) - (this.y2**2 - this.y1**2)),
-                0.5 * ((this.x1**2 - this.x3**2) - (this.y3**2 - this.y1**2)),
-            ];
-        if(Math.abs(math.det([[a, b], [c, d]])) < 1e-6){    //colinear
-            svgElem.setAttribute("cx", 0);
-            svgElem.setAttribute("cy", 0);
-            svgElem.setAttribute("rx", 0);
-            svgElem.setAttribute("ry", 0);
-        }
-        else{
-            const [cx,cy] = 
-                [
-                    -(d * e - b * f) / (b * c - a * d),
-                    -( a * f - c * e) / (b * c - a * d),
+        this.updateMath = function(svgElem){
+            // Calculate circumcenter from three points
+            // Formula: https://blog.csdn.net/liyuanbhu/article/details/52891868
+            const [a, b, c, d, e, f] = 
+                [   
+                    this.x1 - this.x2,
+                    this.y1 - this.y2,
+                    this.x1 - this.x3,
+                    this.y1 - this.y3,
+                    0.5 * ((this.x1**2 - this.x2**2) - (this.y2**2 - this.y1**2)),
+                    0.5 * ((this.x1**2 - this.x3**2) - (this.y3**2 - this.y1**2)),
                 ];
-            svgElem.setAttribute("cx", toPxPosX(cx));
-            svgElem.setAttribute("cy", toPxPosY(cy));
-            svgElem.setAttribute("rx", toPxLenX(math.distance([cx,cy], [this.x1, this.y1])));
-            svgElem.setAttribute("ry", toPxLenY(math.distance([cx,cy], [this.x1, this.y1])));
+            if(Math.abs(math.det([[a, b], [c, d]])) < 1e-6){    //colinear
+                svgElem.setAttribute("cx", 0);
+                svgElem.setAttribute("cy", 0);
+                svgElem.setAttribute("rx", 0);
+                svgElem.setAttribute("ry", 0);
+            }
+            else{
+                const [cx,cy] = 
+                    [
+                        -(d * e - b * f) / (b * c - a * d),
+                        -( a * f - c * e) / (b * c - a * d),
+                    ];
+                svgElem.setAttribute("cx", toPxPosX(cx));
+                svgElem.setAttribute("cy", toPxPosY(cy));
+                svgElem.setAttribute("rx", toPxLenX(math.distance([cx,cy], [this.x1, this.y1])));
+                svgElem.setAttribute("ry", toPxLenY(math.distance([cx,cy], [this.x1, this.y1])));
+            }
         }
     }
 }
