5823b5e9df2190ec81768473f3f0035bd21231d4
Wish Lin <wish.rirf@gmail.com>
2023-01-21 14:21:37 +0800

    start working

---
+++ main.js
@@ -0,0 +1,31 @@
+const {BrowserWindow, app} = require("electron");
+const path = require("path");
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 960,
+        height: 600,
+        minWidth: 640,
+        minHeight: 400,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js")
+        }
+    });
+    win.loadFile("FNGplot.html");
+}
+
+app.whenReady().then( () => {
+    createWindow();
+});
+
+app.on("window-all-closed", () => { // Special case for MacOS
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
+
+app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
