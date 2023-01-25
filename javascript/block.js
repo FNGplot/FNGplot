bb6614eb8dd3664fdaf10856b5e6c5d1dc5d58de
Wish Lin <wish.rirf@gmail.com>
2023-01-25 10:34:06 +0800

    HTML indentation change: 4 spaces to 2 spaces Changed to reduce nesting

--- javascript/block.js
+++ javascript/block.js
@@ -7,8 +7,8 @@ Terminology in my code:
 - FNGobject: FNGobject refers to the smallest functioning unit of the block system. It consists of three parts:
     Object: The core. A JS object describing the math object.
         When created as a blank object, they get a set of default values as properties
-    Sortable block: The input. This is the colorful & draggable blocks that the users see.
-    SVG element: The output. The Object's rendering result on canvas.
+    Sortable block: The input. This is the colorful & draggable blocks that the users manipulate.
+    SVG element (SvgElem): The output. The FNGobject's rendering result.
 
 - SID: Each FNGobject gets its own SID (system ID) upon creation/load.
 
